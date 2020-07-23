fced6fa7dd11b74fd38f2eca73504523f58a03ac
Павел <p-trishkin1@mail.ru>
2020-07-23 20:39:44 +0300

    HomeWork1

--- src/main/java/cloud_client/Client.java
+++ src/main/java/cloud_client/Client.java
@@ -2,9 +2,7 @@ package cloud_client;
 
 import cloud_server.SocketThread;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -24,11 +22,16 @@ public class  Client extends Thread {
             out.writeUTF(id);
             System.out.println(in.readUTF());
             while (true) {
-                System.out.println(id);
                 out.writeUTF(sc.nextLine());
-                System.out.println("byte written");
                 String b = in.readUTF();
                 System.out.println(b);
+                System.out.println(id);
+                if(sc.nextLine().equalsIgnoreCase("U")){
+                    System.out.println("Введите имя файла");
+                    String fileName = "./src/main/java/download/" + sc.nextLine();
+                    sendFile(s, new File(fileName));
+                }
+                System.out.println("byte written");
             }
 
         } catch (IOException e) {
@@ -36,6 +39,26 @@ public class  Client extends Thread {
         }
     }
 
+    public static void sendFile(Socket socket, File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+        long size = file.length();
+        int count = (int) (size / 8192) / 10, readBuckets = 0;
+        // /==========/
+        try(DataOutputStream os = new DataOutputStream(socket.getOutputStream())) {
+            byte [] buffer = new byte[8192];
+            os.writeUTF(file.getName());
+            System.out.print("/");
+            while (is.available() > 0) {
+                int readBytes = is.read(buffer);
+                readBuckets++;
+                if (readBuckets % count == 0) {
+                    System.out.print("=");
+                }
+                os.write(buffer, 0, readBytes);
+            }
+            System.out.println("/");
+        }
+    }
 
     public static void main(String[] args) {
         new Client();
--- src/main/java/cloud_server/Console_server.java
+++ src/main/java/cloud_server/Console_server.java
@@ -15,19 +15,6 @@ public class Console_server{
         while (true){
             socketThread = new SocketThread(server.accept());
         }
-//        String fileName = in.readUTF();
-//        System.out.println("fileName: " + fileName);
-//        File file = new File("./common/server/" + fileName);
-//        file.createNewFile();
-//        try (FileOutputStream os = new FileOutputStream(file)) {
-//            byte[] buffer = new byte[8192];
-//            while (true) {
-//                int r = in.read(buffer);
-//                if (r == -1) break;
-//                os.write(buffer, 0, r);
-//            }
-//        }
-//        System.out.println("File uploaded!");
     }
 
     public static void main(String[] args) throws IOException {
